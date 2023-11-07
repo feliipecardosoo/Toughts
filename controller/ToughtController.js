c2ed67db472f7c72dc07e3d3b1efa7a7f183bd7c
feliipecardosoo <fglibino@gmail.com>
2023-11-07 20:14:42 -0300

    Criacao do ADD pensamentos

--- controller/ToughtController.js
+++ controller/ToughtController.js
@@ -8,4 +8,7 @@ module.exports = class ToughtController {
     static async dashboard (req,res) {
         res.render('toughts/dashboard')
     }
+    static createTought (req,res) {
+        res.render('toughts/create')
+    }
 }
