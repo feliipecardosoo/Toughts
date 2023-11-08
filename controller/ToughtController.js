313fc83023f95b61e35b95375195ec561b9df785
feliipecardosoo <fglibino@gmail.com>
2023-11-08 20:23:30 -0300

    atualizando pensamentos

--- controller/ToughtController.js
+++ controller/ToughtController.js
@@ -75,4 +75,22 @@ module.exports = class ToughtController {
 
         res.render('toughts/edit', {tought})
     }
+    static async updateToughtSave (req,res) {
+        const id = req.body.id
+
+        const tought = {
+            title: req.body.title,
+        }
+       try {
+        await Tought.update(tought, {where: {id:id}})
+
+        req.flash('message', 'Pensamento editado com sucesso!')
+
+        req.session.save(() => {
+            res.redirect('/toughts/dashboard')
+        })
+       } catch (err) {
+        console.log(err)
+       }
+    }
 }
