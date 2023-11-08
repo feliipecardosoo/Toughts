f7bfbdc7c09af359150dadaa4b17222c8222092b
feliipecardosoo <fglibino@gmail.com>
2023-11-08 20:16:27 -0300

    adicionando a edicao

--- controller/ToughtController.js
+++ controller/ToughtController.js
@@ -68,4 +68,11 @@ module.exports = class ToughtController {
             console.log(err)
         }
     }
+    static async updateTought (req,res) {
+        const id = req.params.id
+
+        const tought = await Tought.findOne({where: {id: id}, raw: true })
+
+        res.render('toughts/edit', {tought})
+    }
 }
